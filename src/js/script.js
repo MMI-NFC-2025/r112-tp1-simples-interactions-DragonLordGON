96e79dda924a2757e31eda4acc642f84806e64ed
Merlin HENRY <merlin.henry@edu.univ-fcomte.fr>
2025-11-07 09:27:55 +0100

    Update README and implement click event to change text color

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,14 @@
-const RedClick = document.querySelector('#en-rouge-suite-a-click');
-RedClick.addEventListener('click', (evt)=>{
-    RedClick.style.color="red";
+const enRougeSuiteAClick = document.querySelector('#en-rouge-suite-a-click');
+enRougeSuiteAClick.addEventListener('click', (evt)=>{
+    enRougeSuiteAClick.style.color="red";
 })
+
+const aMettreEnRouge = document.querySelector('#a-mettre-en-rouge');
+aMettreEnRouge.style.color="red";
+
+const CollH2 = document.querySelectorAll('h2');
+CollH2.forEach((h2)=>{
+    h2.addEventListener('click', (evt)=>{
+        h2.style.color="red";
+    });
+});
